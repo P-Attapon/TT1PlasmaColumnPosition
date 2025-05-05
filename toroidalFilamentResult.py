fix DxDz calculation (missing negative sign)

toroidalFilamentResult.py
@@ -7,8 +7,11 @@ from tqdm import tqdm
 #toroidal filament functions
 from process_probe_data import retreive_plasma_current, retreive_magnetic_signal,trim_quantities
 from toroidalFilament_dir.plasma_shift import toroidal_filament_shift_progression
+from toroidalFilament_dir.DxDz import cal_newton_DxDz as cal_DxDz
+from toroidalFilament_dir.geometry_TT1 import coil_angle_dict, all_arrays
 
-shot_lst = list(range(961,968))
+# shot_lst = list(range(961,968))
+shot_lst = [966]
 time_extension = 30 #ms
 
 for shot_no in shot_lst:
@@ -22,10 +25,19 @@ for shot_no in shot_lst:
 
     #calculate shift with toroidal filament
     use_probes = [[1,4,7,10],[12,3,6,9],[11,2,5,8],[12,2,6,8]]
+    # use_probes = [array for array in all_arrays if 11 not in array]
     valid_time, toroidal_R0_arr, toroidal_R0_err, toroidal_Z0_arr, toroidal_Z0_err = toroidal_filament_shift_progression(time,plasma_signal,use_probes)
 
+    all_Dx = [[] for _ in range(len(use_probes))]
+    all_Dz = [[] for _ in range(len(use_probes))]
 
-    def result_plot(axR, axZ, adjust):
+    for signal in plasma_signal.to_numpy():
+        for i,probe_num in enumerate(use_probes):
+            Dx, Dz = cal_DxDz([signal[j] for j in probe_num],[coil_angle_dict[j] for j in probe_num])
+            all_Dx[i].append(Dx)
+            all_Dz[i].append(Dz)
+
+    def result_plot(axR, axZ):
         # toroidal filament result
         for t, R_shift, R_err, probe_arr in zip(valid_time, toroidal_R0_arr, toroidal_R0_err, use_probes):
             line = axR.plot(t, np.array(R_shift), label=f"{probe_arr}")
@@ -53,8 +65,42 @@ for shot_no in shot_lst:
         axZ.set_title("plasma vertical shift")
         axZ.legend(loc="lower center", bbox_to_anchor=(0.5, -0.4), ncol=3)
 
-    fig, ax = plt.subplots(1,2,figsize = (15,5))
-    result_plot(ax[0],ax[1],False)
+    fig, ax = plt.subplots(2,3,figsize = (15,5))
+
+    fig.suptitle(f"result of shot {shot_no}")
+
+    #plasma current
+    column_names = plasma_signal.columns[1:]
+    ax[0,0].plot(time, plasma_current,color = "black")
+    ax[0,0].set_xlabel("time [ms]")
+    ax[0,0].set_ylabel("$I_p$ [A]")
+    ax[0,0].grid()
+    ax[0,0].set_title("plot of $I_p$ agains time")
+
+    cmap = plt.get_cmap("tab20")  # A colormap with 10 distinct colors
+
+    #probe signal
+    for i, name in enumerate(column_names):
+        ax[1,0].plot(time, plasma_signal[name], label=name, color=cmap(i),alpha = 0.8)
+    ax[1,0].set_xlabel("time [ms]")
+    ax[1,0].set_ylabel("$S_i$ [T]")
+    ax[1,0].grid()
+    ax[1,0].set_title("magnetic probe signal")
+    ax[1,0].legend(loc = "lower center", bbox_to_anchor = (0.5,-0.4),ncol = 4,framealpha = 0.3)
+
+    result_plot(ax[0,2],ax[1,2])
+
+    for Dx_probe, Dz_probe,probe_num in zip(all_Dx, all_Dz,use_probes):
+        ax[0,1].plot(time,Dx_probe,label = probe_num)
+        ax[1,1].plot(time,Dz_probe,label = probe_num)
+
+    ax[0,1].set_ylim(-0.4,0.4)
+    ax[0,1].grid()
+    ax[1,1].set_ylim(-0.4,0.4)
+    ax[1,1].grid()
+    ax[1,1].legend(loc="lower center", bbox_to_anchor=(0.5, -0.4), ncol=3)
+
     save_path = r"C:\Users\pitit\Documents\02_MUIC_programming\ICPY_441_Senior_project_in_physics\plasmaColumnPosition\resources\result" + f"\\treeToroidal{shot_no}"
     plt.tight_layout()
+    # plt.show()
     plt.savefig(save_path)

toroidalFilament_dir/DxDz.py
@@ -20,10 +20,8 @@ def cal_newton_DxDz(signal: list[float], c_angle: NDArray[np.float64]) -> list[f
     phi1, phi2 = c_angle[0], c_angle[1]
     frac1, frac2 = (signal[0] - signal[2])/(signal[0] + signal[2]), (signal[1] - signal[3])/(signal[1] + signal[3])
 
-    if np.isinf(frac2): print(signal,np.rad2deg(c_angle))
-
-    Dx = R/np.sin(phi1-phi2) * ( frac1*np.sin(phi2) - frac2*np.sin(phi1) )
-    Dz = R/np.sin(phi1-phi2) * ( -frac1*np.cos(phi2) + frac2*np.cos(phi1) )
+    Dx = -R/np.sin(phi1-phi2) * ( frac1*np.sin(phi2) - frac2*np.sin(phi1) )
+    Dz = -R/np.sin(phi1-phi2) * ( -frac1*np.cos(phi2) + frac2*np.cos(phi1) )
 
     return [Dx,Dz]
 

toroidalFilament_dir/signal_strength.py
@@ -37,7 +37,7 @@ def coil_signal(phi:np.float64, r:float, z:float, a_f:float,I = I):
                 K + E * (a_f ** 2 - r ** 2 - z ** 2) / ((a_f - r) ** 2 + z ** 2)
                 )
 
-    return abs(-b_r(r, z, a_f) * np.sin(phi) + b_z(r, z, a_f) * np.cos(phi)) #absolute because signal is always positive regardless of plasma current vector
+    return -b_r(r, z, a_f) * np.sin(phi) + b_z(r, z, a_f) * np.cos(phi) #absolute because signal is always positive regardless of plasma current vector
 
 
 def cal_signal(horizontal_shift: float, vertical_shift: float, coil_angle: NDArray[np.float64]):